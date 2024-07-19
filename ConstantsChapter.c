4f296ec140e5a316b5e06db40ff630894f2a2961
Joao Pereira <jpcpereirav2@gmail.com>
2024-07-19 17:33:54 +0100

    finished

---
+++ ConstantsChapter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    //constant = fixed value that cannot be altered by the program during its exec
+    // escrever constante em CAPS, bons modos
+    const float PI = 3.14159;
+    
+    printf("%f",PI);
+
+    return 0;
+}
