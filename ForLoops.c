042ca7bad7f9def3fbcf920b2cfcb572fb2a9fd8
Joao Pereira <jpcpereirav2@gmail.com>
2024-07-22 17:10:02 +0100

    finished

--- ForLoops.c
+++ ForLoops.c
@@ -3,7 +3,7 @@
 int main()
 {
 
-    // for loop = repeats a section of code a limited amount of times~
+    // for loop = repeats a section of code a limited amount of times
     for (int i = 10; i >= 1; i-=3)
     {
         printf("%d\n",i);
